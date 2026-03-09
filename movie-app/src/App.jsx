50c3b75d8f087253a704a0ba0338710d707f1044
Aryan Raut <aryansraut29@gmail.com>
2026-03-08 20:38:04 -0700

    base

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <h1> Find Your Next Favorite <span className="text-gradient">Movie</span> without any hassle</h1>
+        </header>
+        <p> search</p>
+      </div>
+    </main>
   )
 }
 
